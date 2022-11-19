52bdd4677058629fee14d337c0438f4f9fc9242c
lijinghao <lijinghao@buaa.edu.com>
2022-11-19 16:40:31 +0800

    dao中函数命名规范，见DemoMapper

--- src/main/java/com/backend/mapper/DemoMapper.java
+++ src/main/java/com/backend/mapper/DemoMapper.java
@@ -11,6 +11,13 @@ import java.util.List;
 @Mapper
 @Repository
 public interface DemoMapper {
+    //方法命名规范
+    //获取单个对象：get*，如getById
+    //获取多个对象：list*，如listById，listAll
+    //插入对象：insert*
+    //删除对象：remove*，如removeById
+    //修改对象：update*，如updateById
+    
     //sql查询函数
     @Select("select * from demo where demo.id=#{id} and demo.name=#{name}")
     Demo getDemo(@Param("id"/*和sql中{}的一致*/) int demoId, @Param("name") String name);
