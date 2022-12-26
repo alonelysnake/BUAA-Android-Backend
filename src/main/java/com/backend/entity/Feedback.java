19e22d1e53daeeb2d67343b8a62e732dd573d3e2
lijinghao <lijinghao@buaa.edu.com>
2022-12-26 16:06:04 +0800

    好友 添加删除 反馈

--- src/main/java/com/backend/entity/Feedback.java
+++ src/main/java/com/backend/entity/Feedback.java
@@ -6,6 +6,11 @@ public class Feedback {
     private int u_id;
     private String fb_content;
     
+    public Feedback(int u_id, String fb_content) {
+        this.u_id = u_id;
+        this.fb_content = fb_content;
+    }
+    
     public int getId() {
         return fb_id;
     }
