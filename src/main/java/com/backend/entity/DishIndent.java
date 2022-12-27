6f5a8003fbeffdbd7d6a3aea5e5e4de7a89d62d6
lijinghao <lijinghao@buaa.edu.com>
2022-12-27 15:13:31 +0800

    订单-菜 关系添加构造方法

--- src/main/java/com/backend/entity/DishIndent.java
+++ src/main/java/com/backend/entity/DishIndent.java
@@ -4,11 +4,20 @@ public class DishIndent {
     private int o_id;
     private int d_id;
     private int sum;
-
+    
+    public DishIndent() {
+    }
+    
+    public DishIndent(int o_id, int d_id, int sum) {
+        this.o_id = o_id;
+        this.d_id = d_id;
+        this.sum = sum;
+    }
+    
     public int getSum() {
         return sum;
     }
-
+    
     public int getOrderId() {
         return o_id;
     }
